de4efb3e9fa64ddc4cec0a6eebab1bd5bbeea8b8
Carsten Wollsen <carsten.wollsen@gmail.com>
2016-07-04 14:40:51 +0200

    renamed the standard program files

---
+++ Publisher/Publisher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+	class Publisher
+	{
+		private static string _broker;
+		private static string _queue;
+		private static string _user;
+		private static string _password;
+
+		public static void Main(string[] args)
+		{
+
+		}
+
+		public static bool Publish(string message)
+		{
+
+
+			return true;
+		}
+	}
+}
--- SonicDeliberations/Consumer/Publisher.cs
+++ SonicDeliberations/Consumer/Publisher.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Consumer
 {
-	class Program
+	class Publisher
 	{
 		static void Main(string[] args)
 		{
